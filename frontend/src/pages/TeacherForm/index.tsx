31c8732615dc38381a997a77ae67dda629603839
asreal089 <lexx_89@hotmail.com>
2020-08-09 22:05:05 -0300

    Add componentes de input

--- frontend/src/pages/TeacherForm/index.tsx
+++ frontend/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
 import PageHeader from '../components/PageHeader';
+import Input from '../components/Input';
+import WarningIcon from '../../assets/images/icons/warning.svg';
+import Textarea from '../components/TextArea';
+import './styles.css';
+import Select from '../components/Select';
 
 function TeacherForm() {
 	return (
-		<div id="page-teacher-list" className="container">
-			<PageHeader title="Que legal que você via dar aulas"></PageHeader>
+		<div id="page-teacher-form" className="container">
+			<PageHeader
+				title="Que legal que você via dar aulas"
+				description="O primeiro passo é preencher sua inscrição:"
+			></PageHeader>
+			<main>
+				<fieldset>
+					<legend>Seus dados:</legend>
+					<Input name="name" label="Nome"></Input>
+					<Input name="avatar" label="Link da sua imagem"></Input>
+					<Input name="whatsapp" label="Whatsapp"></Input>
+					<Textarea name="bio" label="Sua bio"></Textarea>
+				</fieldset>
+
+				<fieldset>
+					<legend>Sobre as aulas:</legend>
+					<Select
+						name="subject"
+						label="materia"
+						options={[
+							{ value: 'Portugues', label: 'Portugues' },
+							{ value: 'Matemática', label: 'Matemática' },
+							{ value: 'Historia', label: 'Historia' },
+							{ value: 'Geografica', label: 'Geografica' },
+							{ value: 'Fisica', label: 'Fisica' },
+							{ value: 'Quimica', label: 'Quimica' },
+							{ value: 'Biologia', label: 'Biologia' },
+						]}
+					></Select>
+					<Input name="cost" label="Preço hora/aula"></Input>
+				</fieldset>
+				<footer>
+					<p>
+						<img src={WarningIcon} alt="Aviso Importante" />
+						Importante <br />
+						Preencha todos os dados.
+					</p>
+					<button type="button">Salvar Cadastro</button>
+				</footer>
+			</main>
 		</div>
 	);
 }
--- frontend/src/pages/TeacherList/index.tsx
+++ frontend/src/pages/TeacherList/index.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import PageHeader from '../components/PageHeader';
-
-import './styles.css';
 import TeacherItem from '../components/TeacherItem';
+import Input from '../components/Input';
+import './styles.css';
 
 function TeacerList() {
 	return (
 		<div id="page-teacher-list" className="container">
 			<PageHeader title="Esses são professores disponiveis no momento.">
 				<form id="search-teachers">
-					<div className="input-block">
-						<label htmlFor="subject">Matéria</label>
-						<input type="text" id="subject" />
-					</div>
-					<div className="input-block">
-						<label htmlFor="week_day">Dia da semana:</label>
-						<input type="text" id="subject" />
-					</div>
-					<div className="input-block">
-						<label htmlFor="time">Hora:</label>
-						<input type="text" id="subject" />
-					</div>
+					<Input name="subject" label="Matéria"></Input>
+					<Input name="week_day" label="Dia da semana"></Input>
+					<Input type="time" name="time" label="Hora"></Input>
 				</form>
 			</PageHeader>
 			<TeacherItem />
